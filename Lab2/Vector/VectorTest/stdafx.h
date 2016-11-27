c0e91f6ec6d8be795f8353a8b85d0f8e339cb762
Vovkarik <solekcsgo@gmail.com>
2016-11-27 16:33:52 +0300

    Проблемы

--- Lab2/Vector/VectorTest/stdafx.h
+++ Lab2/Vector/VectorTest/stdafx.h
@@ -7,12 +7,13 @@
 
 #include <stdio.h>
 #include <tchar.h>
+#include <vector>
+#include <string>
 #define _SCL_SECURE_NO_WARNINGS
 #define BOOST_TEST_INCLUDED
 #pragma warning (disable:4702) // To disable 4702 warning in boost
 #include <boost/test/unit_test.hpp>
 #include <boost/algorithm/string/replace.hpp>
-
 
 
 
