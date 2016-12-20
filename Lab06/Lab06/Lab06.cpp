ba06ef6b43800e5d24fb44902289ec81a60b9973
Vovkarik <solekcsgo@gmail.com>
2016-12-21 00:33:11 +0300

    Добавлена основная программа для отладки

--- Lab06/Lab06/Lab06.cpp
+++ Lab06/Lab06/Lab06.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
-
+#include "HttpUrl.h"
 
 int main()
 {
+	std::string urlstr;
+	std::getline(std::cin, urlstr);
+	CHttpUrl URL(urlstr);
+	std::cout << URL.GetDomain() << std::endl;
+	std::cout << URL.GetProtocol() << std::endl;
+	std::cout << URL.GetPort() << std::endl;
+	std::cout << URL.GetURL() << std::endl;
     return 0;
 }
 
